fd78a8d249b3a6d2d8d27af92257cfb634eed3b6
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-12-21 11:39:51 +0530

    updated data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,16 +2,16 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
+  name: "Vignesh Shetty",
   initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  url: "https://vshetty.dev",
+  location: "Bangalore, IN",
+  locationLink: "https://www.google.com/maps/place/bangalore",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer: Building Scalable Solutions and Pioneering New Tech",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "/me.png",
+  avatarUrl: "https://res.cloudinary.com/vigneshshettyin/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1734761162/IMG_0767_xsnqfn.jpg",
   skills: [
     "React",
     "Next.js",
