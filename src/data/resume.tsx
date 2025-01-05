d78c47c31df57abba075bf24db1899b0b0a993ce
Vignesh Shetty <vigneshshetty.in@gmail.com>
2025-01-05 17:39:34 +0530

    fixed build issues

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,7 +11,7 @@ export const DATA = {
     "Software Engineer: Building Scalable Solutions and Pioneering New Tech",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "https://res.cloudinary.com/vigneshshettyin/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1734761162/IMG_0767_xsnqfn.jpg",
+  avatarUrl: "/me.png",
   skills: [
     "React",
     "Next.js",
